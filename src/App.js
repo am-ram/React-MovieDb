80f01c1204a5521c55598a4616f9f8c0a62d04b1
Sai Ram <75354390+am-ram@users.noreply.github.com>
2021-05-14 16:05:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     results: [],
     selected: {}
   });
-  const apiurl = "http://www.omdbapi.com/?apikey=dfe6d885";
+  const apiurl = "http://www.omdbapi.com/?apikey=7e20705a";
 
   const search = (e) => {
     if (e.key === "Enter") {
